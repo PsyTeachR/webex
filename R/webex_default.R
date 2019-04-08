70c2991943c8ab323c1870310144ac2fb1ea9ef6
Lisa Debruine <lisad@lisad-mac.psy.gla.ac.uk>
2019-04-08 16:24:37 +0100

    Added ... to webex_default

--- R/webex_default.R
+++ R/webex_default.R
@@ -2,13 +2,12 @@
 #'
 #' This function wraps \code{rmarkdown::html_document} to configure compilation to embed the default webex CSS and JavaScript files in the resulting HTML.
 #'
-#' @param toc Whether to include a table of contents.
+#' @param ... Additional function arguments to pass to html_document
 #' @export
-webex_default <- function(toc = FALSE) {
+webex_default <- function(...) {
   css <- system.file("reports/default/webex.css", package = "webex")
   js <- system.file("reports/default/webex.js", package = "webex")
 
-  rmarkdown::html_document(toc = toc,
-                           css = css,
-                           includes = rmarkdown::includes(after_body = js))
+  rmarkdown::html_document(css = css,
+                           includes = rmarkdown::includes(after_body = js), ...)
 }
