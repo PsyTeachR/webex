77695ca3e4dee2cc9e9812e3cb2c2b0b15de6e1d
dalejbarr <dalejbarr@protonmail.com>
2021-09-09 08:17:26 +0100

    prep for cran 2

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  if ("webex" %in% rownames(utils::installed.packages())) {
+  if (base::requireNamespace("webex", quietly = TRUE)) {
     warning("You have an outdated version of this package installed ('webex').\n",
             "To avoid problems, please uninstall the old version using:\n",
             "  remove.packages(\"webex\")")
